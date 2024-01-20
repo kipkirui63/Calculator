35dac13f50d6ad1388143fc83777e1109b843548
kipkirui63 <enocksang8356@gmail.com>
2024-01-20 16:52:59 +0300

    changed input and output to be displayed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,18 +4,24 @@ import './Calculator.css';
 
 const Calculator = () => {
   const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
 
   const handleButtonClick = (value) => {
     if (value === '=') {
       try {
-        setInput(eval(input).toString());
+        const result = eval(input).toString();
+        setInput(`${input} =`);
+        setOutput(result);
       } catch (error) {
         setInput('Error');
+        setOutput('');
       }
     } else if (value === 'C') {
       setInput('');
+      setOutput('');
     } else {
       setInput((prevInput) => prevInput + value);
+      setOutput('');
     }
   };
 
@@ -23,7 +29,10 @@ const Calculator = () => {
 
   return (
     <div className="calculator">
-      <input type="text" value={input} readOnly />
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="output">{output}</div>
+      </div>
       <div className="buttons">
         {buttons.map((button) => (
           <button key={button} onClick={() => handleButtonClick(button)}>
